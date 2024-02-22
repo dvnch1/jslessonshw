0b9f6065183eb4634757633ea9b6f06e56cdd5c5
dvnch1 <hardlfromlcore@gmail.com>
2024-02-22 17:00:37 +0200

    Initial commit

---
+++ homework/homework1.js
@@ -0,0 +1,48 @@
+let ai = "hello";
+console.log(ai)
+let bi = "owu";
+console.log(bi)
+let ce = "com";
+console.log(ce)
+let  d = "ua";
+console.log(d)
+let e = 1;
+console.log(e)
+let f = 10;
+console.log(f)
+let g = -999;
+console.log(g)
+let h = 123;
+console.log(h)
+let y = 3.14;
+console.log(y)
+let j= 16;
+console.log(j)
+let k = true;
+console.log(k)
+let l = false;
+console.log(l)
+
+let firstName = "Danil";
+let lastName = "Iakimenko";
+let middleName = "Sergiyovitch";
+
+let sen = `${lastName} ${firstName} ${middleName}`;
+console.log(sen)
+
+let a = 100;
+console.log(typeof a);
+let b = "100";
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+
+let name1= prompt(":enter your name");
+console.log(name1);
+let surr= prompt(":enter your surrname");
+console.log(surr);
+let age= prompt(":enter your age");
+console.log(age);
+
+
